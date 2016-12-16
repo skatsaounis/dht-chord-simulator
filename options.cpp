3d3e4721b0eb23e3e50dd7074a7d8ebc45b15d36
Thanasis Papoutsidakis <pwnzorbot4000@gmail.com>
2016-12-17 00:48:05 +0200

    cli: Allow node-related commands

--- options.cpp
+++ options.cpp
@@ -6,11 +6,15 @@ using namespace TCLAP;
 
 void Options::parse(int argc, char** argv) try {
     CmdLine cmd("Distributed Systems Emulator", ' ', "0.0.1");
-    vector<string> commands{"status", "start", "terminate", "help"};
+    vector<string> commands{"help", "status", "start", "terminate",
+         "list", "join", "depart", "query", "insert", "delete"};
     ValuesConstraint<string> allowed_commands(commands);
     UnlabeledValueArg<string> command("command",
         "Command to be executed by the Distributed Systems Emulator Daemon (dsemud)",
         true, "help", &allowed_commands, cmd);
+    UnlabeledMultiArg<string> parameters("parameters",
+        "Parameters passed to the executed command",
+        false, "list of parameters", cmd, true);
     cmd.parse(argc, argv);
     if (command.getValue() == "help") cmd.getOutput()->usage(cmd);
 }
