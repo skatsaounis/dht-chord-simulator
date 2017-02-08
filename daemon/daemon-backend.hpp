92d9c6a165351ee1109ebe031b1ce2d67606c4d9
Stamatis Katsaounis <katsaouniss@gmail.com>
2017-02-08 16:58:58 +0200

    Fix several synchronization issues

--- daemon/daemon-backend.hpp
+++ daemon/daemon-backend.hpp
@@ -4,27 +4,24 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
-#include <thread>
-#include <mutex>
 
 class Daemon {
+    int sfd = 0;
     bool _m_is_running = true;
     std::unordered_map<std::string, unsigned> node_ids;
-    mutable std::vector<std::thread> threads;
-    mutable std::timed_mutex node_termination_mutex;
-    mutable std::string _node_being_terminated;
 
     void _send_message(const std::string& node_id, const std::string& msg) const;
     void _send_terminate(const std::string& node_id) const;
-    void _send_terminate_proc(const std::string& node_id) const;
-    void _force_node_cleanup(const std::string& node_id) const;
-    void _force_node_cleanup(const std::string& node_id, const std::string& reason) const;
     std::string _pick_random_node();
     std::string _pick_random_node(const std::string& excluded_node_id);
 
 public:
 
+    Daemon();
+    ~Daemon();
+
     bool is_running() const;
+    std::string get_message() const;
 
     void terminate();
     void init_node(const std::string& node_id, const std::string& replica_factor, const std::string& consistency);
@@ -36,7 +33,6 @@ public:
     void query(const std::string& key);
     void insert(const std::string& key, const std::string& value);
     void remove(const std::string& key);
-    void notify(const std::string& action);
 };
 
 #endif  // _DSEMU_DAEMON_BACKEND_HPP_
