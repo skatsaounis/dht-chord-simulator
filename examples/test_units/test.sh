4bd37dcab3df44f0ea6314692b00d1e9d70b0dbc
Stamatis Katsaounis <katsaouniss@gmail.com>
2017-02-27 19:06:01 +0200

    Fix bugs

--- examples/test_units/test.sh
+++ examples/test_units/test.sh
@@ -1,5 +1,5 @@
 # insert, query experiments
-for consistency in “linear” “eventual”; do
+for consistency in linear eventual; do
     for replica-factor in 1 3 5; do
         dsemu start
         dsemu start -n 1 -r ${replica-factor} -c ${consistency}
@@ -7,8 +7,8 @@ for consistency in “linear” “eventual”; do
             dsemu start -n ${i} -r ${replica-factor} -c ${consistency}
             dsemu join -n ${i}
         done
-        for file in “insert.txt” “query.txt”; do
-            echo “time for consistency: ${consistency} replica-factor: ${replica-factor} Command-file: ${file}”
+        for file in insert.txt query.txt; do
+            echo "time for consistency: ${consistency} replica-factor: ${replica-factor} Command-file: ${file}"
             time test-parser.py ${file}
         done
 
@@ -17,7 +17,7 @@ for consistency in “linear” “eventual”; do
 done
 
 # requests experiments
-for consistency in “linear” “eventual”; do
+for consistency in linear eventual; do
     dsemu start
     dsemu start -n 1 -r 3 -c ${consistency}
     for i in {2..10}; do
@@ -25,7 +25,7 @@ for consistency in “linear” “eventual”; do
         dsemu join -n ${i}
     done
 
-    echo “time for consistency: ${consistency} replica-factor: 3 Command-file: requests.txt”
+    echo "time for consistency: ${consistency} replica-factor: 3 Command-file: requests.txt"
     time test-parser.py requests.txt
     dsemu terminate
 done
