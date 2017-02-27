91457657dd4cd1a01e26a5c6d022755dc9a60565
Thanasis Papoutsidakis <pwnzorbot4000@gmail.com>
2017-02-27 18:57:56 +0000

    Add test unit

---
+++ examples/test_units/test.sh
@@ -0,0 +1,31 @@
+# insert, query experiments
+for consistency in “linear” “eventual”; do
+    for replica-factor in 1 3 5; do
+        dsemu start
+        dsemu start -n 1 -r ${replica-factor} -c ${consistency}
+        for i in {2..10}; do
+            dsemu start -n ${i} -r ${replica-factor} -c ${consistency}
+            dsemu join -n ${i}
+        done
+        for file in “insert.txt” “query.txt”; do
+            echo “time for consistency: ${consistency} replica-factor: ${replica-factor} Command-file: ${file}”
+            time test-parser.py ${file}
+        done
+
+        dsemu terminate
+    done
+done
+
+# requests experiments
+for consistency in “linear” “eventual”; do
+    dsemu start
+    dsemu start -n 1 -r 3 -c ${consistency}
+    for i in {2..10}; do
+        dsemu start -n ${i} -r 3 -c ${consistency}
+        dsemu join -n ${i}
+    done
+
+    echo “time for consistency: ${consistency} replica-factor: 3 Command-file: requests.txt”
+    time test-parser.py requests.txt
+    dsemu terminate
+done
