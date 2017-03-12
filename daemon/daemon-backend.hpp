46fb1339ffdc56a92dc5dac9dae35eb9077155e2
Stamatis Katsaounis <katsaouniss@gmail.com>
2017-03-12 17:00:25 +0200

    Feature/message queue (#2)
    * Implement daemon message queue
    
    This was a bit hardcore. Testing needed.
    
    * Fix bugs in message queue

--- daemon/daemon-backend.hpp
+++ daemon/daemon-backend.hpp
@@ -3,12 +3,71 @@
 
 #include "commands.hpp"
 #include <vector>
+#include <queue>
 #include <string>
 #include <unordered_map>
 
-class Daemon {
+class MessageQueue {
+    bool _m_is_open = false;
     int sfd = 0;
-    bool _m_is_running = true;
+    std::string _m_socket_path;
+    std::deque<std::string> message_queue;
+
+    std::string _get_message() const;
+
+public:
+
+    class iterator
+    {
+    public:
+        typedef unsigned difference_type;
+        typedef std::string value_type;
+        typedef std::string* pointer;
+        typedef std::string& reference;
+        typedef std::input_iterator_tag iterator_category;
+
+        typedef std::deque<value_type> container;
+
+    private:
+        MessageQueue& parent;
+        container& mqueue;
+        container recycled;
+        std::string _m_message;
+        bool _m_has_message = false;
+        bool _m_is_endit = false;
+
+        bool is_end() const;
+        reference message();
+
+    public:
+        static iterator createEnd(MessageQueue& parent_queue, container& msg_queue);
+
+        iterator(MessageQueue& parent_queue, container& msg_queue);
+        ~iterator();
+
+        bool operator==(const iterator&);
+        bool operator!=(const iterator&);
+
+        iterator& operator++();
+        reference operator*();
+        pointer operator->();
+
+        void recycle_message();
+    };
+
+    ~MessageQueue();
+
+    bool is_open() const;
+
+    void open(std::string socket_path);
+    void close();
+
+    iterator begin();
+    iterator end();
+};
+
+
+class Daemon {
     unsigned _m_replica_factor = 0;
     std::unordered_map<std::string, unsigned> node_ids;
 
@@ -19,12 +78,10 @@ class Daemon {
     std::string _pick_random_node(const std::string& excluded_node_id);
 
 public:
+    mutable MessageQueue message_queue;
 
     Daemon();
-    ~Daemon();
 
-    bool is_running() const;
-    std::string get_message() const;
     unsigned get_replica_factor() const;
     void set_replica_factor(unsigned);
 
